6849ddb96aefb406c5f6479f84a195036f8d49fa
mwerner1 <mwerner@calpoly.edu>
2014-01-24 11:37:25 -0800

    Source code for square wave and sine wave with tremolo

---
+++ DateList/DateList/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DateList
+//
+//  Created by Matthew Werner on 1/11/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
